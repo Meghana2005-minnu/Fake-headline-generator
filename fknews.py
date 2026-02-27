586781cdf6057c669d80128dcce5e4541f04859f
meghana2005-minnu <18meghana2005@email.com>
2026-02-27 13:32:05 +0530

    updated subjects,actions and places_or_things

--- fknews.py
+++ fknews.py
@@ -2,15 +2,15 @@ import random
 
 subjects = [
     "Hritik Roshan", "Virat Kohli", "A kerala cat", "A group of dogs", "Prime minister modi", "Ola driver","shah ruk khan"
-    "Dhanush", "3 movie", "Spider man"
+    "Dhanush", "3 movie", "Spider man" , "A group of monkeys", "A group of cows", "A group of buffaloes", "A group of elephants", "A group of lions"
 ]
 
 actions = [
-    "running fast", "dances with", "orders", "eats", "sleeps", "launches", "declares war on", "celebrates", "typing", "smiling with happiness", "by seeing"
+    "running fast", "dances with", "orders", "eats", "sleeps", "launches", "declares war on", "celebrates", "typing", "smiling with happiness", "by seeing","crying after seeing", "laughing after seeing", "jumping with joy after seeing"
 ]
 
 places_or_things = [
-    "at Red Fort", "in Mumbai Local Train", "a plate of kachori", "inside parliament", "at river GANGA", "at India Gate", "during world war"
+    "at Red Fort", "in Mumbai Local Train", "a plate of kachori", "inside parliament", "at river GANGA", "at India Gate", "during world war","at Taj Mahal", "at a wedding", "at a birthday party", "at a cricket match", "at a football match", "at a basketball match", "at a hockey match"
 ]
 
 #headlines
